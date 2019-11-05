2c3eaf10eaf5259d651c7ef902ccb1a5c0093eca
Adam Gajzlerowicz <nelf86@gmail.com>
2019-11-05 17:21:30 +0100

    webview

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, WebView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <WebView
+          source={{ uri: 'https://adamgajzlerowicz.github.io/portfolio' }}
+          style={{ marginTop: 20 }}
+      />
     </View>
   );
 }
@@ -12,8 +15,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
