9ac241a2d9d0309a1f800775cd2a68e6193fcfa4
Adam Gajzlerowicz <nelf86@gmail.com>
2019-11-05 21:30:35 +0100

    asdf

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet, View, WebView } from 'react-native';
+import {Linking} from "expo";
+
+const uri = 'https://adamgajzlerowicz.github.io/portfolio';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <WebView
-          source={{ uri: 'https://adamgajzlerowicz.github.io/portfolio' }}
+          ref={ref => { this.webview = ref; }}
+          source={{ uri }}
           style={{ marginTop: 20 }}
+          onNavigationStateChange={(event) => {
+            if (event.url.indexOf(uri) < 0) {
+              this.webview.stopLoading();
+              Linking.openURL(event.url);
+              return false
+            }
+          }}
+
       />
     </View>
   );
